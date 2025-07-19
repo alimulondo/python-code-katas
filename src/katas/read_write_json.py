4d6871c9f99496a0dec9148cbea3dc87e7bc53eb
Ali <myalimul@gmail.com>
2025-07-19 19:01:04 +0300

    Update read_write_json.py

--- src/katas/read_write_json.py
+++ src/katas/read_write_json.py
@@ -1,11 +1,14 @@
-import json
-
-
-def write_to_json_file(data: dict, file_name: str):
-    with open(file_name, 'w+') as write_file:
-        json.dump(data, write_file, indent=4)
-
-
-def read_from_json_file(file_name: str):
-    with open(file_name, 'r+') as read_file:
-        return json.load(read_file)
+# TODO: Implement functions to write to and read from a JSON file below.
+#
+# What should you do?
+# Write two functions:
+#   1. write_to_json_file(data: dict, file_name: str):
+#        - This function should save the dictionary 'data' to a file with the name 'file_name' in JSON format.
+#   2. read_from_json_file(file_name: str):
+#        - This function should read a JSON file with the name 'file_name' and return its contents as a dictionary.
+#
+# Example usage:
+#   write_to_json_file({'a': 1, 'b': 2}, 'output.json')
+#   data = read_from_json_file('output.json')  # data should be {'a': 1, 'b': 2}
+#
+# Try to write code that follows these rules!
