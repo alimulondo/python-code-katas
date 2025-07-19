cce4f795c27e0ff6cd8440ad8e4f062383c5bc19
Ali <myalimul@gmail.com>
2025-07-19 18:50:07 +0300

    Update fizz_buzz.py

--- src/katas/fizz_buzz.py
+++ src/katas/fizz_buzz.py
@@ -1,15 +1,15 @@
-class FizzBuzz:
-
-    def convert(self, number: int):
-        result = ''
-
-        if number % 3 == 0:
-            result += 'Fizz'
-
-        if number % 5 == 0:
-            result += 'Buzz'
-
-        if result != '':
-            return result
-        else:
-            return number
+# TODO: Implement the FizzBuzz class and its convert method below.
+#
+# What should you do?
+# Create a class called FizzBuzz with a method named convert(self, number: int).
+# The convert method should:
+#   - Return "Fizz" if the number is divisible by 3 (but not 5)
+#   - Return "Buzz" if the number is divisible by 5 (but not 3)
+#   - Return "FizzBuzz" if the number is divisible by both 3 and 5
+#   - Return the original number if it is not divisible by 3 or 5
+#
+# Example usage:
+#   fizz_buzz = FizzBuzz()
+#   result = fizz_buzz.convert(15)  # result should be "FizzBuzz"
+#
+# Try to write code that follows these rules!
